5444dbfb5078782b6f1e4d0d61e3708bd8d65775
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-13 12:41:04 -0400

    Version 0.0.6 create main.js file to add function which the user write something in textarea, lable shows how many characters left

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function(){
+	'use strict';
+
+var commnet = document.getElementById('comment');
+
+var label = document.getElementById('label');
+
+var LIMIT = 30;
+
+label.innerHTML = LIMIT;
+
+
+})();

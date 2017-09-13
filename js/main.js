b722ad10300272108ae68b89266328fe8e3bb919
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-13 13:39:22 -0400

    Version 0.0.8 modified main.js to edit function and modified styles.css

--- js/main.js
+++ js/main.js
@@ -15,13 +15,21 @@ var commnet = document.getElementById('comment');
 var label = document.getElementById('label');
 
 var LIMIT = 30;
-
+var WARNING = 10;
 label.innerHTML = LIMIT;
 
 commnet.addEventListener('keyup',function(){
 	var remaining = LIMIT - this.value.length;
 
 	label.innerHTML= remaining;
+
+/*	if(remaining < WARNING){  
+       label.className = 'warning';
+	}else{
+       label.className = '';
+	}*/
+ label.className = remaining < WARNING ? 'warning' : '';
+
 });
 
 })();
